3c1af0e7ab4126f8c4228717a048e20a48ef2281
aditya singh <adityasingh789m@gmail.com>
2024-12-19 15:36:00 +0530

    hash fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
-import hashPassword from "../helpers/auth.js"
+import { hashPassword, comparePassword } from "../helpers/auth.js";
+
 import jwt from "jsonwebtoken";
 import CleaningRequest from "../models/Cleaningrequest.js";
 import dotenv from "dotenv";
